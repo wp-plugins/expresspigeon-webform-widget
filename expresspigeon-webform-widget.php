02ffc65cbe1f0b135721bed1e05db292c274a363
expresspigeon <expresspigeon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-14 08:22:33 +0000

    1.0 initial commit
    git-svn-id: https://plugins.svn.wordpress.org/expresspigeon-webform-widget/trunk@639045 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ expresspigeon-webform-widget.php
@@ -0,0 +1,73 @@
+<?php
+/*
+Plugin Name: ExpressPigeon webform widget
+Plugin URI: http://expresspigeon.com
+Description: Adds an ExpressPigeon subscription form.
+Version: 1.0
+Min WP Version: 3.0
+Author: Stas Sidorov
+Author URI: http://expresspigeon.com
+*/
+?>
+<?php
+add_action('widgets_init', create_function('', 'return register_widget("ExpressPigeon_Webform_Widget");'));
+add_filter('the_content', 'widget_iframe_on_page', 10, 1);
+add_filter( 'plugin_action_links', 'iframe_plugin_action_links', 10, 2 );
+
+class ExpressPigeon_Webform_Widget extends WP_Widget {
+
+	function __construct() {	   
+		$widget_ops = array('classname' => 'ExpressPigeon_Webform_Widget', 'description' => "ExpressPigeon subscription form widget." );
+		$control_ops = array('width' => 200, 'height' => 300);
+		parent::__construct('iframewidget', __('ExpressPigeon Widget'), $widget_ops, $control_ops);
+	}
+	
+	function form($instance) {
+		$instance = wp_parse_args((array) $instance, array('width' => 400, 'height' => 200, 'guid' => ''));
+		$width = strip_tags($instance['width']);
+		$height = strip_tags($instance['height']);
+		$guid = strip_tags($instance['guid']);
+		$style = strip_tags($instance['style']);
+		?>
+		<p><small>Display an ExpressPigeon subscription form.</small></p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'width' ); ?>"><?php _e( 'Width*:' ); ?></label>
+			<input size="4" id="<?php echo $this->get_field_id('width'); ?>" name="<?php echo $this->get_field_name('width'); ?>" type="text" value="<?php echo esc_attr($width); ?>" />
+	        &nbsp;&nbsp;&nbsp;  
+			<label for="<?php echo $this->get_field_id( 'height' ); ?>"><?php _e( 'Height*:' ); ?></label> 
+			<input size="4" id="<?php echo $this->get_field_id('height'); ?>" name="<?php echo $this->get_field_name('height'); ?>" type="text" value="<?php echo esc_attr($height); ?>" />
+		</p>
+		<p><small>* The Width and Height attributes can be specified either in pixels (example: 50px or simply 50) or as a percentage of the available space (example: 50%).</small></p>		
+		<p>
+			<label for="<?php echo $this->get_field_id( 'guid' ); ?>"><?php _e( 'Form GUID*:' ); ?></label>
+			<input size="40" id="<?php echo $this->get_field_id('guid'); ?>" name="<?php echo $this->get_field_name('guid'); ?>" type="text" value="<?php echo esc_attr($guid); ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'style' ); ?>"><?php _e( 'CSS Style:' ); ?></label>
+			<input size="40" id="<?php echo $this->get_field_id('style'); ?>" name="<?php echo $this->get_field_name('style'); ?>" type="text" value="<?php echo esc_attr($style); ?>" />
+			<br><small>(example:<i>border:1px solid black;align:left;</i>)</small>	
+		</p>	
+		<?php
+	}
+	
+	function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		$instance['width'] = strip_tags($new_instance['width']);
+		$instance['height'] = strip_tags($new_instance['height']);
+		$instance['guid'] = strip_tags($new_instance['guid']);			
+		$instance['style'] = strip_tags($new_instance['style']);		
+		return $instance;
+	}
+
+	// This prints the widget
+	function widget( $args, $instance ) {	
+		extract($args);
+		?>
+		<iframe style="<?php echo $instance['style'] ; ?>" scrolling="auto" frameborder="0" src="https://expresspigeon.com/subscription/iframe_form/<?php echo $instance['guid'] ; ?>" width="<?php echo $instance['width'] ; ?>" height="<?php echo $instance['height'] ; ?>">
+			Your user agent does not support frames or is currently configured not to display frames. However, you can subscribe <a href="https://expresspigeon.com/subscription/form/<?php echo $instance['guid'] ; ?>">here</a>.
+		</iframe>
+		<?php 
+		echo $after_widget; 
+	}	
+}	
+?>
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== ExpressPigeon webform widget ===
+Contributors: expresspigeon
+Tags: iframe,widget,ExpressPigeon,webform
+Requires at least: 3.0
+Tested up to: 3.4.2
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+ExpressPigeon webform widget displays ExpressPigeon webform using iframe.
+
+== Description ==
+ExpressPigeon webform widget displays ExpressPigeon webform using iframe.
+
+== Installation ==
+1. Download and unzip ep-webform-widget.zip
+2. Upload the folder containing `ep-webform-widget.php` to the `/wp-content/plugins/` directory
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. To add an ExpressPigeon webform on a sidebar, browse to `Appearance > Widgets` and add the `ExpressPigeon webform widget` to desired sidebar. Configure webform GUID, height, width and save your changes.
+
+== Frequently Asked Questions ==
+
+= Can I add explicit style to created iframe? =
+
+You can specify explicit styles for iframe in CSS style input in widget configuration
+
+= How do I add an ExpressPigeon webform to a blog-post or Page? =
+
+Login into your ExpressPigeon account, grab 'Iframe content' from your web form and insert it into your post.
+
+== Screenshots ==
+
+1. Configuration for the ExpressPigeon webform widget on sidebar.
+2. Example of created ExpressPigeon webform on sidebar.
+
+== Changelog ==
+
+= 1.0 =
+* Initial public release.
+
+== Upgrade Notice ==
